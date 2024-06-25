8f0bc9bf95b49e5b127f2b29cb4efb0df6bea900
jisanoai <jisanoai@gmail.com>
2024-06-25 11:54:04 +0900

    changed notices.html name to opensource.html and footer.html menu design

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "523396266"
+    serviceWorkerVersion: "445692565"
   }
 });
